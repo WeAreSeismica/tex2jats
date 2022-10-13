d34f479f1c2c53cd6b11930dbf6ea91cb180c487
Thea Ragon <tragon@caltech.edu>
2022-10-12 17:23:52 -0700

    up

--- cleanidjats.py
+++ cleanidjats.py
@@ -10,9 +10,12 @@ import re
 def cleanid(xmlname):
         
     special_characs = "\"#$%&*+/:;<=>?@[\]^`{|}~"
+    special_characs2 = ["<",">","&"]
+    xml_code2=['&#60;','&#62;','&#38;']
+    
     def codepoints(stri):
         out = ''
-        for c in stri:
+        for c in stri: 
             if c in special_characs:
                 out = out+'{:04X}'.format( ord(c) )
             else:
@@ -28,6 +31,20 @@ def cleanid(xmlname):
         xml = fi.read()
     
     xml_new = re.sub('id=\"(.*?)\"', func, xml, flags = re.M)
+        
+    with open(xmlname+'.xml', 'w') as fi:
+        xml = fi.write(xml_new)
+        
+    ## clean special characters
+    with open(xmlname+'.xml', 'r') as fi:
+        xml_new = fi.read()
+    
+    for i,c in enumerate(special_characs2):
+        try:
+            xml_new = re.sub(r' \{} '.format(c), ' {} '.format(xml_code2[i]), xml_new, flags = re.M)
+            xml_new = re.sub(r' {} '.format(c), ' {} '.format(xml_code2[i]), xml_new, flags = re.M)
+        except:
+            pass
         
     with open(xmlname+'.xml', 'w') as fi:
         xml = fi.write(xml_new)
