9eee9a8ba26a1e632f95a598a84d8845500bc27e
AG <abhineetgupta@users.noreply.github.com>
2022-11-11 23:27:33 -0800

    added dockerized setup

---
+++ docker_entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "Running conversion to xml..."
+echo "LATEX_DIR = ${LATEX_DIR}"
+echo "TEX_FILENAME = ${TEX_FILENAME}"
+echo "BIB_FILENAME = ${BIB_FILENAME}"
+
+docker_latex_dir="/data/${LATEX_DIR}"
+
+cp /tmp/tex2xml.sh "$docker_latex_dir/"
+cp /tmp/apa.csl "$docker_latex_dir/"
+cp /tmp/cleanidjats.py "$docker_latex_dir/"
+cp /tmp/cleanxrefjats.py "$docker_latex_dir/"
+cp /tmp/metatex2jats.py "$docker_latex_dir/"
+
+cd "$docker_latex_dir"
+
+sh ./tex2xml.sh "$TEX_FILENAME" "$BIB_FILENAME"
+
+echo "Conversion to xml completed."
+
+exec "$@"
