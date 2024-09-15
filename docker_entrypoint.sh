f9df9283a03c237c52532e3d6fc348057f9951a4
AG <abhineetgupta@users.noreply.github.com>
2024-09-14 19:45:35 -0700

    fixed docker setup

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -9,10 +9,7 @@ docker_latex_dir="/data/${LATEX_DIR}"
 
 cp /tmp/tex2xml.sh "$docker_latex_dir/"
 cp /tmp/apa.csl "$docker_latex_dir/"
-cp /tmp/cleanidjats.py "$docker_latex_dir/"
-cp /tmp/cleanxrefjats.py "$docker_latex_dir/"
-cp /tmp/metatex2jats.py "$docker_latex_dir/"
-
+cp /tmp/cleanjats.py "$docker_latex_dir/"
 cd "$docker_latex_dir"
 
 sh ./tex2xml.sh "$TEX_FILENAME" "$BIB_FILENAME"
